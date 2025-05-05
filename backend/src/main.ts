b477495e212cfa18d528853a823c69319ba394f6
Ahmed Hassan <Ahmed7sn3amer@gmail.com>
2025-05-05 22:55:39 +0300

    change cors env variable

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: `${configService.get<string>("CORS")} || *`,
+    origin: `${configService.get<string>("CORS_ORIGIN")} || *`,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
